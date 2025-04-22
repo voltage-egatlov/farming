7fec26e457a762435f60119d3d2baf6900647251
xechname <nchang1221@gmail.com>
2025-04-22 16:04:54 -0400

    NEW INVENTORY LAYOUT IN Ninas Scene

---
+++ Faming_Unity_Project/Assets/Scripts/InventoryManager_NEW.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class InventoryManager_NEW : MonoBehaviour
+{
+    public GameObject inventoryPanel; // Drag your panel's RectTransform here
+    public TMP_Text balanceText;
+
+    public TMP_Text tomatoSeedText;
+    public TMP_Text carrotSeedText;
+    public TMP_Text cornSeedText;
+    public TMP_Text cabbageSeedText;
+    public TMP_Text wheatSeedText;
+
+    public TMP_Text cornCropText;
+
+    private bool isOpen = false;
+
+
+    public void ToggleInventory()
+    {
+         if (inventoryPanel.activeSelf)
+        {
+            inventoryPanel.SetActive(false);
+        }
+        else
+        {
+            UpdateInventoryDisplay();
+            inventoryPanel.SetActive(true);
+        }
+    }
+
+    private void UpdateInventoryDisplay()
+    {
+        balanceText.text = "Balance: $" + GameManager.Instance.GetCurrentBalance();
+        cornSeedText.text = "Corn Seeds: " + GameManager.Instance.GetSeedCount("Corn");
+        cornCropText.text = "Corn: " + GameManager.Instance.GetCropCount("Corn");
+    }
+
+    void Update()
+    {
+        if (inventoryPanel.activeSelf)
+        {
+            UpdateInventoryDisplay();
+        }
+    }
+}
